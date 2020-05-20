4832a163e589805e79a2767703126654a6fcc656
Rahul Dogra <rahuldogra1998@gmail.com>
2020-05-20 22:41:10 +0530

    express sessions part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -37,14 +39,23 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+//app.use(cookieParser("12345-67890-09876-54321"));
+
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
-  //no user property?
-  if (!req.signedCookies.user) {
-    var authHeader = req.headers.authorization; //req authHeader
-    //authHeader not avaialable
+  console.log(req.session);
+
+  if (!req.session.user) {
+    var authHeader = req.headers.authorization;
     if (!authHeader) {
       var err = new Error("You are not authenticated!");
       res.setHeader("WWW-Authenticate", "Basic");
@@ -52,16 +63,14 @@ function auth(req, res, next) {
       next(err);
       return;
     }
-    //authHeader available
     var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
       .toString()
       .split(":");
     var user = auth[0];
     var pass = auth[1];
     if (user == "admin" && pass == "password") {
-      //autherized user?
-      res.cookie("user", "admin", { signed: true });
-      next();
+      req.session.user = "admin";
+      next(); // authorized
     } else {
       var err = new Error("You are not authenticated!");
       res.setHeader("WWW-Authenticate", "Basic");
@@ -69,11 +78,11 @@ function auth(req, res, next) {
       next(err);
     }
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "admin") {
+      console.log("req.session: ", req.session);
       next();
     } else {
       var err = new Error("You are not authenticated!");
-
       err.status = 401;
       next(err);
     }
